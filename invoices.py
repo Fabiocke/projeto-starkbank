aa020f3bf7a8df8b1a3fd015eda22b13a6daa02b
fabiocke <fabiomt92@hotmail.com>
2021-12-07 03:09:02 -0300

    primeiro commit

--- invoices.py
+++ invoices.py
@@ -62,7 +62,6 @@ class InvoiceCreator:
     # Gera uma lista de faturas pra n clientes
     def get_invoices_customers(self, n):
         customers=self.get_random_customers(n)
-        return
         return self.get_invoices(customers)
     
     # Envia uma lista de faturas de n clinetes
